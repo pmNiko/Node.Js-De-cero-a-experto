c4c15ba0bded760452c7b7d96546deb0990cf810
nikodev <martin.panebianco@smandes.gob.ar>
2024-08-27 10:22:26 -0300

    Patron factory adapter aplicado

---
+++ 02-Bases/src/app.js
@@ -0,0 +1,12 @@
+
+/** Patron factory adapter */
+// const {getUUID, getAge} = require('./plugins')
+// const {buildMakePerson} = require('./js-fundation/07-applied-factory')
+// // factory function applied
+// const buildPerson = buildMakePerson({getUUID, getAge})
+
+// const jhon = buildPerson({name: 'John', birthdate: '1989-01-09'})
+// const jane = buildPerson({name: 'Jane', birthdate: '1990-04-10'})
+
+// console.table({jhon});
+// console.table({jane});
---
+++ 02-Bases/src/js-fundation/01-template.js
@@ -0,0 +1,14 @@
+
+
+const emailTemplate = `
+    <div>
+        <h1>Hi, {{name}}</h1>
+        <p>Thanks you for your order.</p>
+    </div>
+`;
+
+
+
+module.exports = {
+    emailTemplate
+}
