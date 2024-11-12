9c8797647b6235a4ca3888d252d797e084e0668a
nikodev <martin.panebianco@smandes.gob.ar>
2024-11-12 07:35:04 -0300

    inicio de la seccion 20

---
+++ 20-protected-routes/README.md
@@ -0,0 +1,32 @@
+# Rest Project + TypeScript
+
+Este proyecto previamente inicializado tiene todo lo necesario para trabajar con TypeScript, Express y Rest.
+
+Cada paso de su configuración ya se ha realizado previamente en el curso, por lo que solo es necesario clonar el proyecto y comenzar a trabajar.
+
+## Instalación
+
+1. Clonar .env.template a .env y configurar las variables de entorno
+2. Ejecutar `npm install` para instalar las dependencias
+3. En caso de necesitar base de datos, configurar el docker-compose.yml y ejecutar `docker-compose up -d` para levantar los servicios deseados.
+4. Ejecutar `npm run dev` para levantar el proyecto en modo desarrollo
+
+<br/>
+
+---
+
+<br/>
+
+- Puntualmente veremos:
+
+  - Envío de correo
+
+  - Creación de links de retorno
+
+  - ngrok para tunneling
+
+  - Variables de entorno para facilitar sus cambios
+
+  - Pruebas de conexión y validación desde el celular
+
+  - Pruebas hacia el localhost desde internet
---
+++ 20-protected-routes/src/presentation/services/auth.service.ts
@@ -0,0 +1,109 @@
+import { BcryptAdapter, envs, JwtAdapter } from "../../config";
+import { UserModal } from "../../data";
+import { CustomError, RegisterUserDto, UserEntity } from "../../domain";
+import { LoginUserDto } from "../../domain/dtos";
+import { EmailService } from "./email.service";
+
+export class AuthService {
+  constructor(private readonly emailService: EmailService) {}
+
+  public async registerUser(registerUserDTO: RegisterUserDto) {
+    const existUser = await UserModal.findOne({ email: registerUserDTO.email });
+
+    if (existUser) throw CustomError.badRequest("Email already exist");
+
+    try {
+      const user = new UserModal(registerUserDTO);
+
+      user.password = BcryptAdapter.hash(registerUserDTO.password);
+
+      await user.save();
+
+      // Email de confirmacion
+      await this.sendEmailValidationLink(user.email);
+
+      const { password, ...userProps } = UserEntity.fromObject(user);
+
+      const token = await JwtAdapter.generateToken({ id: userProps.id });
+
+      if (!token) throw CustomError.internalServer("Error while creating JWT");
+
+      return {
+        user: userProps,
+        token,
+      };
+    } catch (error) {
+      throw CustomError.internalServer(`${error}`);
+    }
+  }
+
+  public async loginUser(loginUserDTO: LoginUserDto) {
+    const existUser = await UserModal.findOne({ email: loginUserDTO.email });
+
+    if (!existUser)
+      throw CustomError.notFound(`${loginUserDTO.email} not found`);
+
+    try {
+      const isMatching = BcryptAdapter.compare(
+        loginUserDTO.password,
+        existUser.password!
+      );
+
+      if (!isMatching) throw CustomError.forbiden("Not authorized!");
+
+      const { password, ...userEntity } = UserEntity.fromObject(existUser);
+
+      const token = await JwtAdapter.generateToken({ id: existUser.id });
+
+      if (!token) throw CustomError.internalServer("Error while creating JWT");
+
+      return {
+        user: userEntity,
+        token,
+      };
+    } catch (error) {
+      throw CustomError.internalServer(`${error}`);
+    }
+  }
+
+  private sendEmailValidationLink = async (email: string) => {
+    const token = await JwtAdapter.generateToken({ email });
+
+    if (!token) throw CustomError.internalServer("Error getting token");
+
+    const link = `${envs.WEBSERVICES_URL}/auth/validate-email/${token}`;
+
+    const html = `
+      <h1>Validate your email<h1/>
+      <p>Click on the following link to validate your email<p/>
+      <a href="${link}">Validate your email: ${email}<a/>
+    `;
+
+    const options = {
+      to: email,
+      subject: "Validate your email",
+      htmlBody: html,
+    };
+
+    const isSent = await this.emailService.sendEmail(options);
+    if (!isSent) throw CustomError.internalServer("Error sending email");
+
+    return true;
+  };
+
+  public validateEmail = async (token: string) => {
+    const payload = await JwtAdapter.validateToken(token);
+    if (!payload) throw CustomError.unauthorize("Invalid token");
+
+    const { email } = payload as { email: string };
+    if (!email) throw CustomError.internalServer("Email no in token");
+
+    const user = await UserModal.findOne({ email });
+    if (!user) throw CustomError.internalServer("Email not exists");
+
+    user.emailValidated = true;
+    await user.save();
+
+    return true;
+  };
+}
